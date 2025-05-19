dbbeca112b5cc2bf94cd09cac904bb1e10e72bcf
George Christian <george.santos200713@gmail.com>
2025-05-19 02:17:23 -0300

    configuracao: correcoes em configuracoes iniciais do next e arquivos

--- pages/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zHpmm4h7tcx+R/Bh1v+rdMI6mBf5mY+bPcy+9dl2Fkw=\"\n}"
--- pages/.next/static/chunks/pages/index.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/node_modules_next_c1593d6e._.js",
-  "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_2a5cf4bb._.js",
-  "static/chunks/[root-of-the-server]__12e7e4f1._.js",
-  "static/chunks/[root-of-the-server]__59b2688c._.css",
-  "static/chunks/pages_index_5771e187._.js",
-  "static/chunks/pages_index_57db3d30._.js"
-])
